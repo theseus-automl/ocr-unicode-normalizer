fb326db9a97f9da35a94011792461a1b89be4c13
Timur Kasimov <t.kasimov@aventica.ru>
2021-12-07 13:39:33 +0700

    Added check if language is supported

--- src/normalizer.py
+++ src/normalizer.py
@@ -1,6 +1,9 @@
 from pathlib import Path
 from typing import Optional
 
+from pytesseract import get_languages
+
+from src._exceptions import UnsupportedLanguageError
 from src._image_factory import ImageFactory
 from src._ocr import OCR
 
@@ -17,12 +20,27 @@ class Normalizer:
             tessdata_path,
         )
         self._image_factory = ImageFactory(font_path)
+        self._supported_langs = set(get_languages())
 
     def normalize(
         self,
         text: str,
         **kwargs,
     ) -> str:
+        lang = kwargs.get(
+            'lang',
+            None,
+        )
+
+        if lang is None:
+            raise ValueError('target language is not provided')
+
+        if lang not in self._supported_langs:
+            raise UnsupportedLanguageError(
+                f'language "{lang}" is not supported by Tesseract. '
+                f'Possible languages are: {", ".join(self._supported_langs)}'
+            )
+
         image = self._image_factory.from_text(text)
         normalized = self._ocr.recognize(
             image,
